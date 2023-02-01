91026bec90198366a8d79b1ba3ba3c79543acc26
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-01 11:51:53 -0500

    Update index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -9,7 +9,10 @@ Product.belongsTo (Category,{
   foreignKey:'category_id'
 })
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey:'category_id',
+  onDelete:'CASCADE'
+})
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
