d185e35115aa2d17f49edc03f8416587eea3344d
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-01 11:52:36 -0500

    Update index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,15 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+
 Product.belongsTo (Category,{
   foreignKey:'category_id'
 })
+
 // Categories have many Products
+
 Category.hasMany(Product,{
   foreignKey:'category_id',
   onDelete:'CASCADE'
 })
+
 // Products belongToMany Tags (through ProductTag)
+
+Product.belongsToMany (Tag, {through:ProductTag,
+foreignKey:'product_id'})
 
 // Tags belongToMany Products (through ProductTag)
 
