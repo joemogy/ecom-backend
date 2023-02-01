ed8c8debda52ea15fefdc94bde905455038ccad5
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-01 11:40:37 -0500

    Update index.js

--- ecom-backend/models/index.js
+++ ecom-backend/models/index.js
@@ -1,24 +1,28 @@
-const ProductTag = require('./ProductTag');
-
 // Products belongsTo Category
-Category.hasMany(Product,{
-  foreignKey:'category_id',
-  onDelete:'CASCADE'
-})
 
 Product.belongsTo (Category,{
   foreignKey:'category_id'
 })
 
+// Categories have many Products
+
+Category.hasMany(Product,{
+  foreignKey:'category_id',
+  onDelete:'CASCADE'
+})
+
 // Products belongToMany Tags (through ProductTag)
+
 Product.belongsToMany (Tag, {through:ProductTag,
 foreignKey:'product_id'})
 
 // Tags belongToMany Products (through ProductTag)
 
 Tag.belongsToMany (Product,{
-  through: Product_tag,
-  foreignKey:'tag_id'
+  through: product_tag,
+  foreignKey:'tah_id'
 })
+
 module.exports = {
   Product,
+  Category,
