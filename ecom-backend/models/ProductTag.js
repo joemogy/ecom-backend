3713c69889e77bd449737fc4b580352455b5c332
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-01 11:29:07 -0500

    Update ProductTag.js

--- ecom-backend/models/ProductTag.js
+++ ecom-backend/models/ProductTag.js
@@ -5,7 +5,26 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
+  { id:{
+    type:DataTypes.INTEGER,
+    primaryKey:true,
+    allowNull:false,
+    autoIncrement:true
+  },
+  product_id:{
+    type:DataTypes.INTEGER,
+    references:{
+      model:'Product',
+      key:'id'
+    }
+  },
+  tag_id:{
+    type:DataTypes.INTEGER,
+    references:{
+      model:'Tag',
+      key:'id'
+    }
+  }
     // define columns
   },
   {
