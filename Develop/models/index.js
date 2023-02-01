c8731b84d91792f698a25ed9496f6f20ff4a65d2
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-01 11:51:40 -0500

    Update index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,7 +5,9 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo (Category,{
+  foreignKey:'category_id'
+})
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
