e58edf525e6c92d8898853eddb2f7d3ecc63e282
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-01 11:36:41 -0500

    Update server.js

--- ecom-backend/server.js
+++ ecom-backend/server.js
@@ -1,1 +1,9 @@
+app.use(routes);
 
+// sync sequelize models to the database, then turn on the server
+sync.sequelize({force:true}).then(()=>{
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+
+})
