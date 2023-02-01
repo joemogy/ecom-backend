29a07b9399ce313aca7480e7c57746a5aefa2fcf
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-01 11:44:38 -0500

    Delete ecom-backend directory

--- ecom-backend/server.js
+++
@@ -1,9 +0,0 @@
-app.use(routes);
-
-// sync sequelize models to the database, then turn on the server
-sync.sequelize({force:true}).then(()=>{
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  });
-
-})
